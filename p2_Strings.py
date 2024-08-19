157d3d5acb95dee6ff8a033b25b90de504c54850
bhavanishankar10 <bsgcalm10@gmail.com>
2024-08-19 16:15:09 +0530

    2nd commit for p2_Strings.py , added to few examples

--- p2_Strings.py
+++ p2_Strings.py
@@ -20,7 +20,18 @@ print(a[:10])   # Slicing from Start
 print("Reverse string : " , a[::-1])    # No built-in func for reverse string in python
                                         # we use var_name[::1] to reverse a string
 
-replaceString = "ran inside"    # .replace() useed to replace a specified string/char
+# eg 2
+name = "abcdefgh"
+
+print(name[:])
+print(name[::])
+print(name[0:])
+print(name[1:])
+print(name[0:5])
+print(name[:8])
+print(name[0:7])
+
+replaceString = "ran inside"    # .replace() used to replace a specified string/char
 print(replaceString.replace("n","l"))
 
 print(replaceString.split(" "))   # .split() returns a string as a List of items, items ->separated by separator
@@ -40,4 +51,12 @@ balls = 8
 glove_pair = 7
 shop = "There are {0} bats , {1} balls & {2} glovePairs available in shop ." #{} can hold index num as well
 print(shop.format(bats,balls,glove_pair))   # .format() -> can hold multiple arguments identified with
-                                             # with index number
+                                            # with index number
+
+# f-strings (string formatting)
+fruits = 3
+vegetables = 6
+dryFruits = 2
+storeName = "Dmart"
+grocery = f"There are {fruits} varities of fruits , {vegetables} varieties of vegetables & {dryFruits} varieties of dry fruits in {storeName} ."     # f is used here infront of a string literal & {value} used as placeholders.
+print(grocery)                      # With f strings {} cannot be empty !!
